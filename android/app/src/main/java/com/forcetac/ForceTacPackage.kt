d6f602ef1b6cb2caf4b233a396a3cc6d5a2559b3
Oxsilaris06 <nicolas.maheux45@gmail.com>
2026-01-10 00:21:25 +0100

    Create ForceTacPackage.kt

---
+++ android/app/src/main/java/com/forcetac/ForceTacPackage.kt
@@ -0,0 +1,20 @@
+package com.forcetac
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class ForceTacPackage : ReactPackage {
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        val modules = ArrayList<NativeModule>()
+        // C'est ici qu'on "branche" le module NFC
+        modules.add(NfcModule(reactContext))
+        return modules
+    }
+}
