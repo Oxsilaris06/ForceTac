6b1b88db210540cb5a4aa819fcd92adf1164a721
Oxsilaris06 <nicolas.maheux45@gmail.com>
2026-01-10 13:06:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// 1. Les polyfills DOIVENT être le tout premier import
+import './polyfills'; 
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-// FIX CRITIQUE : Assure que le nom correspond à MainActivity.java
-// Si app.json contient "name": "ForceTac", cela fonctionnera.
+// Enregistrement du composant
 AppRegistry.registerComponent(appName, () => App);
